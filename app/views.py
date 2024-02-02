27228fb06fe88fad23d68999ece56b4b7ed30535
divyavani42000 <divyavani42000@gmail.com>
2024-02-02 13:49:37 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -37,3 +37,42 @@ def selfjoin(request):
     empmgrobjects=Emp.objects.select_related('mgr').order_by(Length('ename').desc())
     d={'empmgrobjects':empmgrobjects}
     return render(request,'selfjoin.html',d)
+
+def emp_mgr_dept(request):
+    emdobjects=Emp.objects.select_related('deptno','mgr').all()
+    emdobjects=Emp.objects.select_related('deptno','mgr').filter(deptno__dname='research')
+    emdobjects=Emp.objects.select_related('deptno','mgr').filter(mgr__ename='blake')
+    emdobjects=Emp.objects.select_related('deptno','mgr').filter(ename='allen')
+    emdobjects=Emp.objects.select_related('deptno','mgr').filter(ename__startswith='a')
+    emdobjects=Emp.objects.select_related('deptno','mgr').filter(ename__endswith='th')
+    emdobjects=Emp.objects.select_related('deptno','mgr').filter(sal__gte=3000)
+    emdobjects=Emp.objects.select_related('deptno','mgr').filter(sal__lte=3000)
+    emdobjects=Emp.objects.select_related('deptno','mgr').filter(ename='martin')
+    emdobjects=Emp.objects.select_related('deptno','mgr').filter(job__in=('manager','analyst'))
+    emdobjects=Emp.objects.select_related('deptno','mgr').filter(ename__in=('smith','allen'))
+    emdobjects=Emp.objects.select_related('deptno','mgr').filter(comm__isnull=True)
+    emdobjects=Emp.objects.select_related('deptno','mgr').filter(comm__isnull=False)
+    emdobjects=Emp.objects.select_related('deptno','mgr').filter(comm__isnull=True)
+    emdobjects=Emp.objects.select_related('deptno','mgr').filter(deptno__in=(10,20))
+    emdobjects=Emp.objects.select_related('deptno','mgr').order_by('-comm')
+    emdobjects=Emp.objects.select_related('deptno','mgr').order_by('comm')
+    emdobjects=Emp.objects.select_related('deptno','mgr').filter(deptno__dname='sales',ename='allen')
+    emdobjects=Emp.objects.select_related('deptno','mgr').order_by('-sal')
+    emdobjects=Emp.objects.select_related('deptno','mgr').order_by('sal')
+    emdobjects=Emp.objects.select_related('deptno','mgr').filter(Q(deptno__dname='research')|Q(mgr__ename='jones'))
+    emdobjects=Emp.objects.select_related('deptno','mgr').order_by(Length('job').desc())
+    emdobjects=Emp.objects.select_related('deptno','mgr').filter(mgr__isnull=True)
+    emdobjects=Emp.objects.select_related('deptno','mgr').filter(mgr__isnull=False)
+    emdobjects=Emp.objects.select_related('deptno','mgr').filter(sal__lt=3000)
+    emdobjects=Emp.objects.select_related('deptno','mgr').filter(sal__gt=3000)
+    emdobjects=Emp.objects.select_related('deptno','mgr').filter(deptno=10)
+    emdobjects=Emp.objects.select_related('deptno','mgr').order_by(Length('job'))
+    emdobjects=Emp.objects.select_related('deptno','mgr').filter(deptno__gt=10)
+    emdobjects=Emp.objects.select_related('deptno','mgr').filter(deptno__dname='sales')
+    emdobjects=Emp.objects.select_related('deptno','mgr').all()[:5:]
+    emdobjects=Emp.objects.select_related('deptno','mgr').all()[2:5:]
+
+
+
+    d={'emdobjects':emdobjects}
+    return render(request,'emp_mgr_dept.html',d)
