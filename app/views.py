3241edcf1ad6ef9650761ecd309a830f622074b4
divyavani42000 <divyavani42000@gmail.com>
2024-02-02 12:38:01 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from app. models import *
+from django.db.models.functions import Length
+from django.db.models import Q
 # Create your views here.
 def equijoin(request):
     empobjects=Emp.objects.select_related('deptno').all()
@@ -16,3 +18,22 @@ def equijoin(request):
     empobjects=Emp.objects.select_related('deptno').filter(ename='smith')
     d={'empobjects':empobjects}
     return render(request,'equijoin.html',d)
+
+def selfjoin(request):
+    empmgrobjects=Emp.objects.select_related('mgr').all()
+    empmgrobjects=Emp.objects.select_related('mgr').filter(sal__gt=1000)
+    empmgrobjects=Emp.objects.select_related('mgr').filter(sal__lt=1000)
+    empmgrobjects=Emp.objects.select_related('mgr').filter(mgr__ename='king')
+    empmgrobjects=Emp.objects.select_related('mgr').filter(mgr__isnull=True)
+    empmgrobjects=Emp.objects.select_related('mgr').filter(mgr__isnull=False)
+    empmgrobjects=Emp.objects.select_related('mgr').filter(ename__endswith='th')
+    empmgrobjects=Emp.objects.select_related('mgr').filter(ename__startswith='a')
+    empmgrobjects=Emp.objects.select_related('mgr').filter(sal__gte=2000)
+    empmgrobjects=Emp.objects.select_related('mgr').filter(sal__lte=3000)
+    empmgrobjects=Emp.objects.select_related('mgr').filter(ename__in=('smith','allen'))
+    empmgrobjects=Emp.objects.select_related('mgr').filter(ename='blake')
+    empmgrobjects=Emp.objects.select_related('mgr').order_by('-sal')
+    empmgrobjects=Emp.objects.select_related('mgr').order_by('sal')
+    empmgrobjects=Emp.objects.select_related('mgr').order_by(Length('ename').desc())
+    d={'empmgrobjects':empmgrobjects}
+    return render(request,'selfjoin.html',d)
